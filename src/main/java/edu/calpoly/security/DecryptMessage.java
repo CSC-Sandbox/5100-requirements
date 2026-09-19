c41b6c5b2fa85ec74c285774e4603984c8acb227
hwrd22 <howardjiang2015@gmail.com>
2026-09-19 15:02:12 -0700

    Class file and documentation for decrypting encrypted messages.

---
+++ src/main/java/edu/calpoly/security/DecryptMessage.java
@@ -0,0 +1,23 @@
+package edu.calpoly.security;
+
+import edu.calpoly.provided.Encryption;
+
+import java.util.Objects;
+import java.util.Scanner;
+
+public class DecryptMessage {
+    public static void main(String[] args) {
+        System.out.println("Type an encrypted message to decrypt it, or type \"/quit\" to quit.");
+        String encryptedMessage = "";
+        Scanner scanner = new Scanner(System.in);
+        while (!Objects.equals(encryptedMessage, "/quit")) {
+            encryptedMessage = scanner.nextLine();
+            if (!Objects.equals(encryptedMessage, "/quit")) {
+                String decryptedMessage = Encryption.decrypt(encryptedMessage);
+                System.out.println("Encrypted: " + encryptedMessage);
+                System.out.println("Decrypted: " + decryptedMessage);
+            }
+        }
+        scanner.close();
+    }
+}
