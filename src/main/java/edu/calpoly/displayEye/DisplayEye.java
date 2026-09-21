17e37ddbaaabfe6d2a79dcdc6150c509636463c7
James Yaguma <jyaguma@gmail.com>
2026-09-21 08:17:50 -0700

    Add some comments to DisplayEye.java

--- src/main/java/edu/calpoly/displayEye/DisplayEye.java
+++ src/main/java/edu/calpoly/displayEye/DisplayEye.java
@@ -17,14 +17,17 @@ public class DisplayEye {
     public static void main(String[] args) {
         DisplayEye displayEye = new DisplayEye();
 
+        // Initial frame setup
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(displayEye.width, displayEye.height);
         frame.setResizable(false);
 
+        // Add the GUI to the frame before turning the frame to visible
         displayEye.gui.addToFrame(frame);
         frame.setVisible(true);
 
+        // Start receiving mesages
         displayEye.broker.loopForever(displayEye.gui::update);
     }
 }
