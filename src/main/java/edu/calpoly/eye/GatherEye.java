e4b96d445be0fbe451c3e455dec48c906fef5eca
Briggs William Murphy <briggswilliammurphy@Mac-2817.lan>
2026-09-21 16:54:21 -0700

    Send gaze data through Broker

--- src/main/java/edu/calpoly/eye/GatherEye.java
+++ src/main/java/edu/calpoly/eye/GatherEye.java
@@ -6,8 +6,11 @@ import javax.swing.JPanel;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
+import edu.calpoly.provided.Broker;
+
 public class GatherEye {
     public static void main(String[] args) {
+        Broker broker = new Broker("localhost", 5000);
         JFrame frame = new JFrame("Gather Eye");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(800, 600);
@@ -22,6 +25,8 @@ public class GatherEye {
                 int y = e.getY();
                 double normalizedX = (double) x / screen.getWidth();
                 double normalizedY = (double) y / screen.getHeight();
+                String message = "GAZE," + normalizedX + "," + normalizedY;
+                broker.send(message);
                 gazeLabel.setText("Gaze X: " + normalizedX + ", Gaze Y: " + normalizedY);
                 System.out.println("X: " + normalizedX + ", Y: " + normalizedY);
             }
