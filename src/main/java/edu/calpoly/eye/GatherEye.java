d032b468abc2e179ce95eaa88f386bc61b94ad80
Briggs William Murphy <briggswilliammurphy@Mac-2817.lan>
2026-09-21 16:31:34 -0700

    Implement gaze position detection

--- src/main/java/edu/calpoly/eye/GatherEye.java
+++ src/main/java/edu/calpoly/eye/GatherEye.java
@@ -2,6 +2,8 @@ package edu.calpoly.eye;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class GatherEye {
     public static void main(String[] args) {
@@ -10,6 +12,17 @@ public class GatherEye {
         frame.setSize(800, 600);
 
         JPanel screen = new JPanel();
+
+        screen.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                int x = e.getX();
+                int y = e.getY();
+
+                System.out.println("X: " + x + ", Y: " + y);
+            }
+        });
+
         frame.add(screen);
 
         frame.setVisible(true);
