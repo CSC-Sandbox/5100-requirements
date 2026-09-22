1dad889ce305f3583cf5f756dbd2a1447b26ebab
Edgard Aviles <edgardaviles05@gmail.com>
2026-09-21 22:29:22 -0700

    Updated implementation follows design.

--- src/main/java/edu/calpoly/storage/RetrieveMessages.java
+++ src/main/java/edu/calpoly/storage/RetrieveMessages.java
@@ -1,20 +1,25 @@
 package edu.calpoly.provided;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.List;
 
 public class RetrieveMessages {
   public static void main(String[] args) {
     Broker broker = new Broker("localhost", 5000);
+    FileMessageReader reader = new FileMessageReader("data/empty-messages.csv");
 
     try {
-      List<String> records = Files.readAllLines(Path.of("data/empty-messages.csv"));
+      List<String> records = reader.readAll();
+      boolean sentAny = false;
+
       for (String r : records) {
         if (!r.isBlank()) {
           broker.send(r);
+          sentAny = true;
         }
+      }
+      if (!sentAny) {
+        System.out.println("No stored messages found");
       }
     } catch (IOException e) {
       System.out.println("Could not read stored messages");
