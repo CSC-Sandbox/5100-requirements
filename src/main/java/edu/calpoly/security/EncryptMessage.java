e55954ce162d849d702deafb5fa2854fc0dbc2d6
hwrd22 <howardjiang2015@gmail.com>
2026-09-24 20:43:42 -0700

    Added Javadocs and implemented small refactoring changes to prevent method overriding in EncryptMessage.

--- src/main/java/edu/calpoly/security/EncryptMessage.java
+++ src/main/java/edu/calpoly/security/EncryptMessage.java
@@ -5,7 +5,14 @@ import edu.calpoly.provided.Encryption;
 import java.util.Objects;
 import java.util.Scanner;
 
-public class EncryptMessage {
+/**
+ * Message encryption implementation for CSC 5100
+ * This class is used to encrypt messages.
+ *
+ * @author Howard Jiang (hwrd22)
+ * @version 1.0 (2026-09-21)
+ */
+public final class EncryptMessage {
     public static void main(String[] args) {
         System.out.println("Type a message to encrypt it, or type \"/quit\" to quit.");
         String message = "";
@@ -21,7 +28,12 @@ public class EncryptMessage {
         scanner.close();
     }
 
-    // Utility method to be used by decryption program
+    /**
+     * Message decryption implementation for CSC 5100
+     * Utility method to encrypt a message in the form of a string.
+     *
+     * @author Howard Jiang (hwrd22)
+     */
     public static String encryptMessage(String message) {
         return Encryption.encrypt(message);
     }
