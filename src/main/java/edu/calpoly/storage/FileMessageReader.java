1a4b70eafb21282f5116d7537e9f66aadf98ad4e
Edgard Aviles <edgardaviles05@gmail.com>
2026-09-25 02:13:04 -0700

    created the evolution of storage feature

--- src/main/java/edu/calpoly/storage/FileMessageReader.java
+++ src/main/java/edu/calpoly/storage/FileMessageReader.java
@@ -5,15 +5,25 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
 
-
+/*
+ * This class reads the entire file storing messages in one instance
+ * It is responsible for just reading our message file
+ *
+ * @author Edgard Aviles
+ * @version September 25, 2026
+ */
 public class FileMessageReader {
 
   private String fileName;
   
+  // @param fileName name of the file holding the messages
   public FileMessageReader(String fileName) {
     this.fileName = fileName;
   }
 
+  /*
+   * @return list of all of the messages in fileName
+   */
   public List<String> readAll() throws IOException {
     return Files.readAllLines(Path.of(fileName));
   }
--- src/main/java/edu/calpoly/storage/FileMessageStore.java
+++ src/main/java/edu/calpoly/storage/FileMessageStore.java
@@ -4,13 +4,27 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+/*
+ * This class is implemented to store the messages received
+ * It is responsible to write/store a message record once received.
+ *
+ * @author Edgard Aviles
+ * @version September 25, 2026
+ */
+
 public class FileMessageStore {
   private final String fileName;
 
+  /*
+   * @param fileName name of the file we are writing to
+   */
   public FileMessageStore(String fileName) {
     this.fileName = fileName;
   }
 
+  /*
+   * @param record the message we want to store
+   */
   public void store(MessageRecord record) throws IOException {
     try (PrintWriter writer = new PrintWriter(new FileWriter(fileName, true))) {
       writer.println(record.toFileLine());
